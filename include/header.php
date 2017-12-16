333c1c480607533ba7a780af797c8b90534f3fb6
Didik Sazali <didiksazali@gmail.com>
2017-12-16 14:49:05 +0700

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 	<div class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="index.php" class="navbar-brand">SMK Hasanah</a>
+          <a href="index.php" class="navbar-brand">Aplikasi Penjualan Sawit</a>
           <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -13,7 +13,7 @@
             <li><a href="index.php"><i class='fa fa-home'></i> Beranda</a></li>
             <li><a href="petani.php"><i class='fa fa-users'></i> Data Petani</a></li>
             <li><a href="sawit.php"><i class='fa fa-file'></i> Data Sawit</a></li>
-            <li><a href="transaksi.php"><i class='fa fa-plus'></i> Tambahkan Data Transaksi</a></li>
+            <li><a href="transaksi.php"><i class='fa fa-plus'></i> Tambah Data Transaksi</a></li>
             <li><a href="laporan.php"><i class='fa fa-file'></i> Laporan</a></li>
           </ul>
 
