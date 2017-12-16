25835a6f5aac3eccad73af18f2db37086849447d
Didik Sazali <didiksazali@gmail.com>
2017-12-16 14:26:35 +0700

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,7 @@
 					<div id="bottom-footer" style='background:#28292b'>
 					<div class='container'>
 					<div class='row'>
-					<span id="copy-text" class="pull-left no-float-xs block-xs align-center-xs">&copy; <?php echo date("Y"); ?>  All Rights Reserved - Create by <a href='#'>SMK Hasanah</span>
+					<span id="copy-text" class="pull-left no-float-xs block-xs align-center-xs">&copy; <?php echo date("Y"); ?>  All Rights Reserved - Made With Love Code by <a href='#'>Didik Sazali</span>
 					<ul id="footer-links" class="pull-right no-float-xs block-xs align-center-xs hidden-xs">
 					</ul>
 					</div>
